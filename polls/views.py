6b7fef7dda534d9f4de9dbf20aa7ccbca22c459f
yunyu <1287641566@qq.com>
2021-10-18 19:33:06 +0800

    url路径传参数

--- polls/views.py
+++ polls/views.py
@@ -17,25 +17,30 @@ def index(request):
 class indexView(View):
     """
     主页类试图"""
-    def get(self,request):
-        # return HttpResponse("Hello, GET")
-        #从数据库查询数据
-        data=[
-            {
-                "project_name":"1" ,
-                "appname":"python"
-            },
-            {
-                "project_name": "2",
-                "appname": "python1"
-            },
-            {
-                "project_name": "3",
-                "appname": "python2"
-            },
-        ]
-        return render(request,'index.html',locals())
+    # def get(self,request):
+    #     # return HttpResponse("Hello, GET")
+    #     #从数据库查询数据
+    #     data=[
+    #         {
+    #             "project_name":"1" ,
+    #             "appname":"python"
+    #         },
+    #         {
+    #             "project_name": "2",
+    #             "appname": "python1"
+    #         },
+    #         {
+    #             "project_name": "3",
+    #             "appname": "python2"
+    #         },
+    #     ]
+    #     return render(request,'index.html',locals())
+
+    def get(self,request,pk):
+        return HttpResponse("Hello, GET")
+
     def post(self,request):
+
         return HttpResponse("Hello, POST")
     def delete(self,request):
         return HttpResponse("Hello, delete")
