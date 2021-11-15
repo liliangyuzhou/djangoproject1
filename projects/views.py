74ce511126f6394081a446f345f499595d28a321
yunyu <1287641566@qq.com>
2021-11-15 14:14:07 +0800

    drf框架：使用GenericViewSet来合并两个类视图，并且通过 ViewSet的as_view(）来实现请求方法和action的一一对应

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse, Http404
 from rest_framework.views import APIView
-#继承APIView，给查询列表提供排序，过滤，分页
+# 继承APIView，给查询列表提供排序，过滤，分页
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -14,6 +14,8 @@ import json
 from projects import serializer
 from rest_framework.viewsets import ModelViewSet
 
+from rest_framework import viewsets
+
 from rest_framework.filters import SearchFilter
 from rest_framework.filters import OrderingFilter
 from rest_framework.pagination import PageNumberPagination
@@ -21,25 +23,26 @@ from rest_framework.pagination import PageNumberPagination
 # from utils import mixins
 from rest_framework import mixins
 from rest_framework import generics
-class ProjectList(generics.ListCreateAPIView):
-    # GenericAPIView是APIView的子类，具有APIView的所有功能
-    #使用GenericAPIView必须要指定queryset和serializer_class两个类属性
-    #queryset是当前类视图函数所需要的查询集
-    #serializer_class是对应的序列化器
-    queryset=Project.objects.all()
-    serializer_class=serializer.ProjectModelSerializer
-    #指定模型序列化器类那些字段需要过滤，可以在字段前面添加前缀，^（必须以什么开头），=，@，$ ，在源码中可以看到
-    #该字段的列表里面的值必须和模型序列化器类的属性名称保持一致，那些参数需要进行过滤，列表中就写那些参数
-    search_fields=['name','tester','id']
 
-    #a.可以在全局配置文件中，指定对应的搜索引擎（对所有获取数据列表的功能都会支持过滤功能）
-    #b.也可以在特定的类视图中指定filter_backends过滤
-    filter_backends = [SearchFilter, OrderingFilter]
-    ordering_fields=['name','tester','id']
 
-    #为了不全局配置搜索引擎，只对某个类视图生效，可以通过pagination_class指定搜索引擎
-    pagination_class =PageNumberPagination
-
+# class ProjectList(generics.ListCreateAPIView):
+#     # GenericAPIView是APIView的子类，具有APIView的所有功能
+#     # 使用GenericAPIView必须要指定queryset和serializer_class两个类属性
+#     # queryset是当前类视图函数所需要的查询集
+#     # serializer_class是对应的序列化器
+#     queryset = Project.objects.all()
+#     serializer_class = serializer.ProjectModelSerializer
+#     # 指定模型序列化器类那些字段需要过滤，可以在字段前面添加前缀，^（必须以什么开头），=，@，$ ，在源码中可以看到
+#     # 该字段的列表里面的值必须和模型序列化器类的属性名称保持一致，那些参数需要进行过滤，列表中就写那些参数
+#     search_fields = ['name', 'tester', 'id']
+#
+#     # a.可以在全局配置文件中，指定对应的搜索引擎（对所有获取数据列表的功能都会支持过滤功能）
+#     # b.也可以在特定的类视图中指定filter_backends过滤
+#     filter_backends = [SearchFilter, OrderingFilter]
+#     ordering_fields = ['name', 'tester', 'id']
+#
+#     # 为了不全局配置搜索引擎，只对某个类视图生效，可以通过pagination_class指定搜索引擎
+#     pagination_class = PageNumberPagination
 
 
 # class ProjectDetail(View):
@@ -109,7 +112,58 @@ class ProjectList(generics.ListCreateAPIView):
 #     serializer_class = ProjectModelSerializer
 
 
-class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
+# class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Project.objects.all()
+#     serializer_class = serializer.ProjectModelSerializer
+
+
+#如果两个类视图合并，一定要继承ViewSet
+# 会出现一些问题？
+#两个get方法会有冲突
+#如果使用mixin中提供的拓展方法（action），当前drf是无法识别这些action
+
+#1。继承ViewSet，
+#ViewSet继承ViewSetMixin, views.APIView
+#就支持这些action，并在定义url路由时，可以在as_view({'固定方法名'：'action名称'})
+#这里所有的action需要自己定义，并且不具有分页，过滤等功能
+# class ProjectViewSet(viewsets.ViewSet):
+# 2。继承GenericViewSet
+#ViewSetMixin, generics.GenericAPIView
+#支持分页，过滤，排序等功能
+#仅仅继承viewsets.GenericViewSet但是还是没有这些action，如何拿到action呢，还需要继承其他类
+# class ProjectViewSet(viewsets.GenericViewSet):
+
+#可以继续继承各种mixin，同时继承viewsets.GenericViewSet，这个类放到后面
+#可以按照需求继承mixin
+class ProjectViewSet(mixins.RetrieveModelMixin,
+                     mixins.CreateModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.ListModelMixin,
+                     mixins.DestroyModelMixin,
+                     viewsets.GenericViewSet):
     queryset = Project.objects.all()
     serializer_class = serializer.ProjectModelSerializer
+    # 指定模型序列化器类那些字段需要过滤，可以在字段前面添加前缀，^（必须以什么开头），=，@，$ ，在源码中可以看到
+    # 该字段的列表里面的值必须和模型序列化器类的属性名称保持一致，那些参数需要进行过滤，列表中就写那些参数
+    search_fields = ['name', 'tester', 'id']
 
+    # a.可以在全局配置文件中，指定对应的搜索引擎（对所有获取数据列表的功能都会支持过滤功能）
+    # b.也可以在特定的类视图中指定filter_backends过滤
+    filter_backends = [SearchFilter, OrderingFilter]
+    ordering_fields = ['name', 'tester', 'id']
+
+    # 为了不全局配置搜索引擎，只对某个类视图生效，可以通过pagination_class指定搜索引擎
+    pagination_class = PageNumberPagination
+
+    # def list(self, request, *args, **kwargs):
+    #     pass
+    # def create(self, request, *args, **kwargs):
+    #     pass
+    # def update(self, request, *args, **kwargs):
+    #     pass
+    # def partial_update(self,request,*args,**kwargs):
+    #     pass
+    # def retrieve(self, request, *args, **kwargs):
+    #     pass
+    # def destroy(self, request, *args, **kwargs):
+    #     pass
