5d3555525a9217cf819edb4fe454339cad1e7f97
yunyu <1287641566@qq.com>
2021-11-15 22:55:56 +0800

    drf框架：在一个类视图中使用多个序列化器，重写get_serializer_class（）来实现，获取一个项目下的所有接口的全部字段

--- projects/serializer.py
+++ projects/serializer.py
@@ -101,7 +101,7 @@ class ProjectModelSerializer(serializers.ModelSerializer):
     # 如果从表外健指定了related_name="interfaces"，使用interfaces，没有的话字段名称使用全小写的从表名_set,固定写法
     # many=True,一对多一定要写many=True，此时PrimaryKeyRelatedField类型返回值是id列表
     # interfaces=serializers.PrimaryKeyRelatedField(read_only=True,many=True)
-    interfaces_set = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
+    # interfaces_set = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
 
     #如果要不要展示接口的的id列表，而是名称.并且只要输出结果有多个，一定要加many=True
     # interfaces_set = serializers.StringRelatedField(many=True)
@@ -111,7 +111,7 @@ class ProjectModelSerializer(serializers.ModelSerializer):
 
     #我们还可以拿到从表的序列化器类，作为一个父表的一个字段，因为继承关系,都是继承自filed，这样这个序列化器输出几个字段，那么
     #interfaces_set里面就包含几个字段
-    # interfaces_set=InterfacesModelSerializer(label="所属接口的信息",many=True)
+    interfaces_set=InterfacesModelSerializer(label="所属接口的信息",many=True)
 
     class Meta:
         # 1.指定参考哪一个模型类来创建
@@ -159,7 +159,10 @@ class ProjectNameSerializer(serializers.ModelSerializer):
         fields = ('name',)
 
 class ProjectInterfacesSerializer(serializers.ModelSerializer):
-    interfaces_set = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
+    #单独输出一个项目下所有接口的id
+    # interfaces_set = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
+    #输出一个项目下所有接口的详细信息
+    interfaces_set = InterfacesModelSerializer(label="所属接口的信息", many=True)
     class Meta:
         model=Project
         fields = ('interfaces_set',)
